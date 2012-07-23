772e0b408139cbab28514a6a357d6fda8428b965
nathanbricault@gmail.com <nathanbricault@gmail.com@b08fb58c-6971-b36e-a1a6-ed6be25b3643>
2012-07-23 18:33:27 +0000

    biclops control

---
+++ trunk/PanTile/Bisight_server/build/CMakeFiles/BiclopsServer.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/noam/ros_workspace/PanTilt/Bisight_server/src/BiclopsAccess.cpp" "/home/noam/ros_workspace/PanTilt/Bisight_server/build/CMakeFiles/BiclopsServer.dir/src/BiclopsAccess.o"
+  "/home/noam/ros_workspace/PanTilt/Bisight_server/src/BiclopsServer.cpp" "/home/noam/ros_workspace/PanTilt/Bisight_server/build/CMakeFiles/BiclopsServer.dir/src/BiclopsServer.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
